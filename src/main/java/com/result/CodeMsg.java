5cf3f11aac574c3af4cc81e5c41d81a28dd1cf63
yangsheng <1821332070@qq.com>
2018-07-20 17:57:16 +0800

    异常类示范-杨胜

--- src/main/java/com/result/CodeMsg.java
+++ src/main/java/com/result/CodeMsg.java
@@ -10,12 +10,19 @@ public class CodeMsg {
 	
 	
 	//登录模块 1002XX
+	public static CodeMsg LOGIN_NAME_OR_PSWD_ERROR = new CodeMsg(100201, "用户名或密码不正确");
+	public static CodeMsg LOGIN_NAME_NOT_EXIST = new CodeMsg(100202, "用户名不存在");
+	public static CodeMsg LOGIN_SUCCESS = new CodeMsg(100203, "用户登录成功");
 	
 	//商品模块 1003XX
+	public static CodeMsg PRODUCT_NOT_EXIST = new CodeMsg(100301, "商品信息不存在");
+	
 	
 	//订单模块 1004XX
+
 	
 	//文件模块 1005XX
+	
 	
 	
 	private CodeMsg(int code, String msg) {
