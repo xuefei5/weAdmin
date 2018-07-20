7beccd1fdcfade17e0395fce846c45223d4b722c
yangsheng <1821332070@qq.com>
2018-07-20 22:43:25 +0800

    用户对象持久层-杨胜

--- src/main/java/com/dao/interfaces/IUserDAO.java
+++ src/main/java/com/dao/interfaces/IUserDAO.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.bean.User;
 
@@ -17,10 +18,22 @@ public interface IUserDAO{
 	public int insert(User user);
 	
 	/*根据id查询用户对象*/
-	@Select("select * from user where id = #{id}")
+	@Select("select * from user where id = #{id} and state='1'")
 	public User qryById(@Param("id")int id	);
 	
 	/*查询所有用户对象*/
-	@Select("select * from user")
+	@Select("select * from user where state='1'")
 	public List<User> qryAll();
+	
+	/*更新用户对象*/
+	@Update("UPDATE user SET name = #{name},password = #{password},telephone = #{telephone},nickName = #{nickName},remarks = #{remarks},registerTime = #{registerTime} wehere id = #{id}")
+	public int update();
+	
+	/*删除用户对象*/
+	@Update("UPDATE user SET name = #{name},password = #{password},telephone = #{telephone},nickName = #{nickName},remarks = #{remarks},registerTime = #{registerTime} wehere state='0'")
+	public int delete();
+	
+	/*根据用户名和密码查询用户对象*/
+	@Select("select * from user where name = #{name} and password = #{password} and state='1'")
+	public User qryByNameAndPswd(@Param("name")String name	,@Param("password")String password	);
 }
