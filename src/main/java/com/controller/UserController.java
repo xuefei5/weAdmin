ac44520badf8978f162558bc92840866bbccddba
yangsheng <1821332070@qq.com>
2018-07-22 07:50:27 +0800

    thymeleaf框架单级多级数据解析样例-杨胜

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -167,5 +168,5 @@ public class UserController {
  		
  		return Result.success(userList);
  	}
-
+ 
 }
