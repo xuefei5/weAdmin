294bb7ccc061933e272810be75f2168a6208da6e
xuefei5 <%Aa123456>
2018-07-23 09:30:01 +0800

    xuefei --登录修改逻辑

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -21,64 +21,72 @@ import com.service.interfaces.IUserSV;
 
 @Controller
 @RequestMapping("/user")
-public class UserController extends BaseController{
-	
-	//日志输出
-	private static final transient Logger logger = Logger.getLogger(UserController.class);
-	
+public class UserController extends BaseController {
+
+	// 日志输出
+	private static final transient Logger logger = Logger
+			.getLogger(UserController.class);
+
 	@Autowired
 	IUserSV userSV;
-	
+
 	/**
 	 * 分页查询用户
+	 * 
 	 * @author yangsheng
 	 */
 	@RequestMapping("/qryUserByPageNum")
-    @ResponseBody
-    public Result<List<User>> qryUserByPageNum(HttpServletRequest request) {
-		
-		int pageNum = Integer.parseInt(null == request.getParameter("pageNum")?"1":request.getParameter("pageNum"));
-		List<User> userList=userSV.qryUserByPageNum(pageNum);
- 		return Result.success(userList);
-    } 
-	
+	@ResponseBody
+	public Result<List<User>> qryUserByPageNum(HttpServletRequest request) {
+
+		int pageNum = Integer
+				.parseInt(null == request.getParameter("pageNum") ? "1"
+						: request.getParameter("pageNum"));
+		List<User> userList = userSV.qryUserByPageNum(pageNum);
+		return Result.success(userList);
+	}
+
 	/**
 	 * 查询所有用户
+	 * 
 	 * @author yangsheng
 	 */
 	@RequestMapping("/qryAllUser")
-    @ResponseBody
-    public Result<List<User>> qryAllUser() {
-		List<User> userList=userSV.qryAllUser();
- 		return Result.success(userList);
-    } 
+	@ResponseBody
+	public Result<List<User>> qryAllUser() {
+		List<User> userList = userSV.qryAllUser();
+		return Result.success(userList);
+	}
 
 	/**
 	 * 进入登陆页面
+	 * 
 	 * @author xuefei
 	 * */
- 	@RequestMapping(value="/toLoginPage", method=RequestMethod.GET)
-    public String toLoginPage() {
-        return "login";
-    }
- 	
- 	/**
+	@RequestMapping(value = "/toLoginPage", method = RequestMethod.GET)
+	public String toLoginPage() {
+		return "login";
+	}
+
+	/**
 	 * 进入登陆页面
+	 * 
 	 * @author yangsheng
 	 * */
- 	@RequestMapping(value="/toMainPage", method=RequestMethod.GET)
-    public String toMainPage() {
- 		if(userSV.isLoin()) {
- 			return "main";
- 		}
-        return "login";
-    }
- 	
- 	/**
- 	 * 用户登录
- 	 * @author yangsheng
- 	 * */
- 	@RequestMapping(value="/login")
+	@RequestMapping(value = "/toMainPage", method = RequestMethod.GET)
+	public String toMainPage() {
+		if (userSV.isLoin()) {
+			return "main";
+		}
+		return "login";
+	}
+
+	/**
+	 * 用户登录
+	 * 
+	 * @author yangsheng
+	 * */
+	@RequestMapping(value="/login")
  	@ResponseBody
  	public Result<CodeMsg> login(HttpServletRequest request){
  		
@@ -88,122 +96,135 @@ public class UserController extends BaseController{
  		
  		logger.info("用户"+name+"在尝试进行登录操作");
  		
- 		if(userSV.login(name, password)) {
- 			logger.info("用户"+name+"在尝试登录成功");
- 			return Result.success(CodeMsg.LOGIN_SUCCESS);
- 		}else {
- 			logger.info("用户"+name+"在尝试登录失败");
+ 		List<User> listUser = userSV.qryByName(name);
+ 		
+        if(null==listUser||listUser.size()<=0){//用户不存在
  			return Result.error(CodeMsg.LOGIN_NAME_NOT_EXIST);
  		}
- 	}
- 	
- 	/**
- 	 * 欢迎页面
- 	 * @author yangsheng
- 	 * */
- 	@RequestMapping(value="/welcome")
- 	public String welcome(){
- 		return "welcome";
- 	}
- 	
- 	/**
- 	 * 用户页面
- 	 * @author yangsheng
- 	 * */
- 	@RequestMapping(value="/user")
- 	public String user(){
- 		return "user";
- 	}
- 	
- 	/**
- 	 * 增加用户页面
- 	 * @author yangsheng
- 	 * */
- 	@RequestMapping(value="/addUserHtml")
- 	public String addUserHtml(){
- 		return "addUser";
- 	}
- 	
- 	/**
- 	 * 用户注销
- 	 * @author yangsheng
- 	 */
- 	@RequestMapping(value="/logout")
- 	public String logout(){
- 		
- 		if(userSV.logout()) {
- 			logger.info("用户注销成功，跳转到登录页面");
- 			return "login";
- 		}else {
- 			logger.info("用户注销失败，情况几乎不可能，直接返回登录页面");
- 			return "login";
+        boolean isLogin = userSV.login(name, password);
+ 		if(!isLogin) {//用户名或者密码错误
+ 			return Result.error(CodeMsg.LOGIN_NAME_OR_PSWD_ERROR);
  		}
- 	}
- 	
- 	/**
- 	 * 用户添加
- 	 * @author yangsheng
- 	 */
- 	@RequestMapping(value="/addUser")
- 	@ResponseBody
- 	public Result<CodeMsg> addUser(User user){
  		
- 		if(userSV.addUser(user)) {
- 			logger.info("用户添加成功");
- 			return Result.success(CodeMsg.USER_ADD_SUCCESS);
- 		}else {
- 			logger.info("用户添加失败");
- 			return Result.error(CodeMsg.USER_ADD_FAIL);
- 		}
- 	}
- 	
- 	/**
- 	 * 用户删除
- 	 * @author yangsheng
- 	 */
- 	@RequestMapping(value="/deleteUser")
- 	@ResponseBody
- 	public Result<CodeMsg> deleteUser(HttpServletRequest request){
+ 		return Result.success(CodeMsg.LOGIN_SUCCESS);
  		
- 		int id = Integer.parseInt(request.getParameter("id"));
- 		if(userSV.deleteUser(id)) {
- 			logger.info("用户删除成功");
- 			return Result.success(CodeMsg.USER_DELETE_SUCCESS);
- 		}else {
- 			logger.info("用户删除失败");
- 			return Result.error(CodeMsg.USER_DELETE_FAIL);
- 		}
  	}
- 	
- 	/**
- 	 * 用户更新
- 	 * @author yangsheng
- 	 */
- 	@RequestMapping(value="/updateUser")
- 	@ResponseBody
- 	public Result<CodeMsg> updateUser(User user){
- 		
- 		if(userSV.updateUser(user)) {
- 			logger.info("用户更新成功");
- 			return Result.success(CodeMsg.USER_UPDATE_SUCCESS);
- 		}else {
- 			logger.info("用户更新失败");
- 			return Result.error(CodeMsg.USER_UPDATE_FAIL);
- 		}
- 	}
- 	
- 	/**
- 	 * 姓名查询用户
- 	 * @author yangsheng
- 	 */
- 	@RequestMapping(value="/qryByName")
- 	@ResponseBody
- 	public Result<List<User>> qryByName(HttpServletRequest request){
- 		
- 		String name = request.getParameter("name");
- 		List<User> userList = userSV.qryByName(name);
- 		
- 		return Result.success(userList);
- 	}
- 
+
+	/**
+	 * 欢迎页面
+	 * 
+	 * @author yangsheng
+	 * */
+	@RequestMapping(value = "/welcome")
+	public String welcome() {
+		return "welcome";
+	}
+
+	/**
+	 * 用户页面
+	 * 
+	 * @author yangsheng
+	 * */
+	@RequestMapping(value = "/user")
+	public String user() {
+		return "user";
+	}
+
+	/**
+	 * 增加用户页面
+	 * 
+	 * @author yangsheng
+	 * */
+	@RequestMapping(value = "/addUserHtml")
+	public String addUserHtml() {
+		return "addUser";
+	}
+
+	/**
+	 * 用户注销
+	 * 
+	 * @author yangsheng
+	 */
+	@RequestMapping(value = "/logout")
+	public String logout() {
+
+		if (userSV.logout()) {
+			logger.info("用户注销成功，跳转到登录页面");
+			return "login";
+		} else {
+			logger.info("用户注销失败，情况几乎不可能，直接返回登录页面");
+			return "login";
+		}
+	}
+
+	/**
+	 * 用户添加
+	 * 
+	 * @author yangsheng
+	 */
+	@RequestMapping(value = "/addUser")
+	@ResponseBody
+	public Result<CodeMsg> addUser(User user) {
+
+		if (userSV.addUser(user)) {
+			logger.info("用户添加成功");
+			return Result.success(CodeMsg.USER_ADD_SUCCESS);
+		} else {
+			logger.info("用户添加失败");
+			return Result.error(CodeMsg.USER_ADD_FAIL);
+		}
+	}
+
+	/**
+	 * 用户删除
+	 * 
+	 * @author yangsheng
+	 */
+	@RequestMapping(value = "/deleteUser")
+	@ResponseBody
+	public Result<CodeMsg> deleteUser(HttpServletRequest request) {
+
+		int id = Integer.parseInt(request.getParameter("id"));
+		if (userSV.deleteUser(id)) {
+			logger.info("用户删除成功");
+			return Result.success(CodeMsg.USER_DELETE_SUCCESS);
+		} else {
+			logger.info("用户删除失败");
+			return Result.error(CodeMsg.USER_DELETE_FAIL);
+		}
+	}
+
+	/**
+	 * 用户更新
+	 * 
+	 * @author yangsheng
+	 */
+	@RequestMapping(value = "/updateUser")
+	@ResponseBody
+	public Result<CodeMsg> updateUser(User user) {
+
+		if (userSV.updateUser(user)) {
+			logger.info("用户更新成功");
+			return Result.success(CodeMsg.USER_UPDATE_SUCCESS);
+		} else {
+			logger.info("用户更新失败");
+			return Result.error(CodeMsg.USER_UPDATE_FAIL);
+		}
+	}
+
+	/**
+	 * 姓名查询用户
+	 * 
+	 * @author yangsheng
+	 */
+	@RequestMapping(value = "/qryByName")
+	@ResponseBody
+	public Result<List<User>> qryByName(HttpServletRequest request) {
+
+		String name = request.getParameter("name");
+		List<User> userList = userSV.qryByName(name);
+
+		return Result.success(userList);
+	}
+
 }
