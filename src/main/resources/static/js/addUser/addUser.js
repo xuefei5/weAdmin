94281c542355509811fac930eb758acc6f1b78ff
yangsheng <1821332070@qq.com>
2018-07-28 13:20:16 +0800

    用户添加，页面刷新修改-杨胜

--- src/main/resources/static/js/addUser/addUser.js
+++ src/main/resources/static/js/addUser/addUser.js
@@ -16,17 +16,24 @@ function addUser(){
 				+'","remarks":"' + $("textarea[name='remarks']").val() + '"}'; 
 	$.ajax({
 		type: "post",
-		async: true,
+		forceSync : false,
         url: "/user/addUser",
         contentType: "application/json; charset=utf-8",
         data: data,
         dataType: "json",
         success: function (message) {
         	if(message.code != '100602'){
-        		layer.open({
-        			title : '提示',
-        			content : "用户添加成功"
-        		});
+        		//点击确认关闭该层
+				var index = parent.layer.getFrameIndex(window.name);
+				layer.msg('保存成功', {
+					icon : 1,
+					time : 1200
+				//1.2秒关闭（如果不配置，默认是3秒）
+				}, function() {
+					//关闭弹出层并且刷新父页面
+					parent.layer.close(index);
+					parent.location.reload();
+				});
         	}else{
         		layer.open({
         			title : '提示',
