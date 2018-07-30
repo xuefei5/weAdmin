5734647dbb61c8fe62a30b00f5d06b829202cbbf
yangsheng <1821332070@qq.com>
2018-07-30 09:37:24 +0800

    商品持久层基类-杨胜

--- src/main/java/com/dao/interfaces/IProductDAO.java
+++ src/main/java/com/dao/interfaces/IProductDAO.java
@@ -21,10 +21,6 @@ public interface IProductDAO{
 	@Select("select * from product where id = #{id} and state='1'")
 	public Product qryById(@Param("id")int id);
 	
-	/*根据商品名模糊查询商品对象*/
-	@Select("select * from product where name like CONCAT('%',#{name},'%') and state='1'")
-	public List<Product> qryByName(@Param("name")String name );
-
 	/*更新商品对象*/
 	@Update("UPDATE product SET name = #{name},tip = #{tip},price = #{price},imgRef = #{imgRef} where id = #{id}")
 	public int update(Product product);
@@ -34,10 +30,18 @@ public interface IProductDAO{
 	public int delete(@Param("id")int id);
 	
 	/*分页查询商品对象*/
-	@Select("select * from product where state='1' order by registerTime desc limit #{start} , #{end} ")
-	public List<Product> qryProductByPageNum(@Param("start")int id,@Param("end")int end);
+	@Select("select * from product where state='1' order by addTime desc limit #{start} , #{end} ")
+	public List<Product> qryProductByPageNum(@Param("start")int start,@Param("end")int end);
 
 	/*查询商品数据条数*/
-	@Select("select count(*) from product")
+	@Select("select count(*) from product where state='1'")
 	public int qryProductCount();
+	
+	/*根据商品名分页模糊查询商品对象*/
+	@Select("select * from product where name like CONCAT('%',#{name},'%') and state='1' order by addTime desc limit #{start} , #{end} ")
+	public List<Product> qryProductByNamePageNum(@Param("name")String name,@Param("start")int start,@Param("end")int end);
+	
+	/*查询商品数据条数*/
+	@Select("select count(*) from product where name like CONCAT('%',#{name},'%') and state='1' ")
+	public int qryProductByNameCount();
 }
