2bec4c983d86d05913644b30791d6333eff18291
xuefei5 <179583637@qq.com>
2018-08-18 16:00:18 +0800

    xuefei --客户信息查看页面除了查看订单详情其余的完全具备

--- src/main/java/com/controller/ContactController.java
+++ src/main/java/com/controller/ContactController.java
@@ -20,9 +20,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
 import com.bean.Contact;
 import com.bean.Order;
+import com.bean.User;
 import com.result.CodeMsg;
 import com.result.Result;
 import com.service.interfaces.IContactSV;
@@ -51,10 +54,21 @@ public class ContactController extends BaseController{
 	 */
 	@RequestMapping(value = "/addContact",method = RequestMethod.POST)
 	@ResponseBody
-	public Result<CodeMsg> addContact(Contact Contact,HttpServletRequest req) {
+	public Result<CodeMsg> addContact(HttpServletRequest req) {
 		try{
+			String inputStr = super.getInputString(req);
+			Contact contact = JSON.parseObject(inputStr, new TypeReference<Contact>() {});
+			//联系时间--格式转换
+			String contactTime = CommonUtil.fomatDate(contact.getContactTime(), "MM/dd/yyyy", "yyyy-MM-dd HH:mm:ss");
+			contact.setContactTime(contactTime);
+			
+			//预约时间--格式转换
+			String subscribeTime = CommonUtil.fomatDate(contact.getSubscribeTime(), "MM/dd/yyyy", "yyyy-MM-dd HH:mm:ss");
+			contact.setSubscribeTime(subscribeTime);
+			
+			logger.info("添加联系信息"+contact.getContactTime());
 
-		if (iContactSV.addContact(Contact)) {
+		if (iContactSV.addContact(contact)) {
 			logger.info("联系记录添加成功");
 			return Result.success(CodeMsg.CANTACT_ADD_SUCCESS);
 		} else {
@@ -70,6 +84,25 @@ public class ContactController extends BaseController{
 			return Result.error(new CodeMsg(101404, e.getMessage()));
 		}
 		
+	}
+	
+	/**
+	 * 联系信息删除
+	 * 
+	 * @author xuefei
+	 */
+	@RequestMapping(value = "/deleteContact")
+	@ResponseBody
+	public Result<CodeMsg> deleteContact(HttpServletRequest request) {
+		JSONObject jsonObj = super.getInputObject(request);
+		int id = Integer.parseInt(jsonObj.getString("id"));
+		if (iContactSV.deleteContact(id)) {
+			logger.info("联系信息删除成功");
+			return Result.success(CodeMsg.CANTACT_DELETE_SUCCESS);
+		} else {
+			logger.info("联系信息删除失败");
+			return Result.error(CodeMsg.CANTACT_DELETE_SUCCESS);
+		}
 	}
 	
 	/**
--- src/main/resources/static/js/customer/seeCustomer.js
+++ src/main/resources/static/js/customer/seeCustomer.js
@@ -4,100 +4,247 @@
  */
 
 //最大上传文件的大小设定
-var FILE_MAX_SIZE = 4*1024*1024;
+var FILE_MAX_SIZE = 4 * 1024 * 1024;
 
-layui.use('upload', function() {});
+layui.use('upload', function() {
+});
 //获取参数方法
 function getUrlParam(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-    if (r != null) return unescape(r[2]); return null; //返回参数值
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+	var r = window.location.search.substr(1).match(reg); //匹配目标参数
+	if (r != null)
+		return unescape(r[2]);
+	return null; //返回参数值
 }
 //根据客户ID获取到客户信息
-	var id = getUrlParam("id");
-	
-	var data ='{ "id":"' + id + '"}';
+var id = getUrlParam("id");
+
+var data = '{ "id":"' + id + '"}';
+$.ajax({
+	type : "post",
+	async : false,
+	url : "/cust/qryCustAndOrderByCustId",
+	contentType : "application/json; charset=utf-8",
+	data : data,
+	dataType : "json",
+	success : function(message) {
+		if (message.code == 0) {
+			var data = message.data;
+			var customer = data.customer;
+			var tdSex = "";
+			if (customer.sex == 0) {// 男
+				tdSex = '男';
+			} else {
+				tdSex = '女';
+			}
+			$("#id").html(customer.id);
+			$("#name").html(customer.name);
+			$("#sex").html(tdSex);
+			$("#telephone").html(customer.telephone);
+			//请求联系记录
+			getContactInfo();
+			//显示订单信息
+			disPlayOrderInfo(data.orderList);
+		} else {
+			layuiAlert(message.msg);
+		}
+		return true;
+	},
+	error : function() {
+		layuiAlert("系统环境异常");
+		return false;
+	}
+});
+//请求并且显示联系记录
+function getContactInfo() {
+
+	var data = '{ "id":"' + id + '"}';
 	$.ajax({
-		type: "post",
-		async: false,
-        url: "/cust/qryCustAndOrderByCustId",
-        contentType: "application/json; charset=utf-8",
-        data: data,
-        dataType: "json",
-        success: function (message) {
-        	if(message.code == 0){
-        		var data = message.data;
-        		var customer = data.customer;
-        		var tdSex ="";
-        		if (customer.sex == 0) {// 男
-        			tdSex = '男';
-        		} else {
-        			tdSex = '女';
-        		}
-        		$("#id").html(customer.id);
-        		$("#name").html(customer.name);
-        		$("#sex").html(tdSex);
-        		$("#telephone").html(customer.telephone);
-        		//显示订单信息
-        		disPlayOrderInfo(data.orderList);
-        	}else{
-        		layuiAlert(message.msg);
-        	}
-        	return true;
-        },
-        error:function(){
-        	layuiAlert("系统环境异常");
-        	return false;
-        }
-  });
-	
+		type : "post",
+		async : false,
+		url : "/contact/qryContactInfoByCustId",
+		contentType : "application/json; charset=utf-8",
+		data : data,
+		dataType : "json",
+		success : function(message) {
+			if (message.code == 0) {
+				var data = message.data;
+				disPlayContactInfo(data);
+			} else {
+				layuiAlert(message.msg);
+			}
+			return true;
+		},
+		error : function() {
+			layuiAlert("系统环境异常");
+			return false;
+		}
+	});
+}
+
+//显示联系记录信息
+function disPlayContactInfo(contactList) {
+	var html = '';
+	$
+			.each(
+					contactList,
+					function(i, item) {
+
+						var r = Number(i);
+						var className = "";
+						// 如果是偶数
+						if (r % 2 == 0) {
+							className = "even"
+						} else {
+							className = "odd"
+						}
+						var trHead = '<tr class="' + className + '">';
+						var tdContactTime = '<td class="sorting_1">'
+								+ item.contactTime + '</td>';
+						var tdContent = '<td class="center">' + item.content
+								+ '</td>';
+						// 是否有机会
+						var tdIsCancel = "";
+						if (item.isChance == 1) {
+							tdIsCancel = '<td class="center">' + '是' + '</td>';
+						} else {
+							tdIsCancel = '<td class="center">' + '否' + '</td>';
+						}
+						var tdSubTime = '<td class="center">'
+								+ item.subscribeTime + '</td>';
+						var btn = '<td class="center "><a class="btn btn-danger" href="#" onClick="deleteContactInfo('
+								+ item.id
+								+ ')"><i class="halflings-icon white trash"></i></a></td>';
+						var trTail = '</tr>';
+						html += trHead + tdContactTime + tdContent + tdIsCancel
+								+ tdSubTime + btn + trTail;
+
+					});
+
+	$("#tbodyContact").html(html);
+}
 //显示订单信息
-	function disPlayOrderInfo(orderList){
-		var html = '';
-		$.each(orderList,function(i, item) {
+function disPlayOrderInfo(orderList) {
+	var html = '';
+	$
+			.each(
+					orderList,
+					function(i, item) {
 
-							var r = Number(i);
-							var className = "";
-							// 如果是偶数
-							if (r % 2 == 0) {
-								className = "even"
-							} else {
-								className = "odd"
-							}
-							var trHead = '<tr class="'
-									+ className + '">';
-							var tdid = '<td class="sorting_1">'
-									+ item.id + '</td>';
-							var tdOderTime = '<td class="center">'
-								+ item.ordertime + '</td>';
-							var tdTotal = '<td class="center">'
-								+ item.total + '</td>';
-							// 是否取消
-							var tdIsCancel ="";
-							if (item.isCancel == 0) {
-								tdIsCancel = '<td class="center">'
-										+ '是' + '</td>';
-							} else {
-								tdIsCancel = '<td class="center">'
-										+ '否' + '</td>';
-							}
-							var tdProductName = '<td class="center">'
+						var r = Number(i);
+						var className = "";
+						// 如果是偶数
+						if (r % 2 == 0) {
+							className = "even"
+						} else {
+							className = "odd"
+						}
+						var trHead = '<tr class="' + className + '">';
+						var tdid = '<td class="sorting_1">' + item.id + '</td>';
+						var tdOderTime = '<td class="center">' + item.ordertime
+								+ '</td>';
+						var tdTotal = '<td class="center">' + item.total
+								+ '</td>';
+						// 是否取消
+						var tdIsCancel = "";
+						if (item.isCancel == 0) {
+							tdIsCancel = '<td class="center">' + '是' + '</td>';
+						} else {
+							tdIsCancel = '<td class="center">' + '否' + '</td>';
+						}
+						var tdProductName = '<td class="center">'
 								+ item.productName + '</td>';
-							var btn = '<td class="center "><a class="btn btn-success" href="#" onClick="seeOrderInfo('+item.id+')"><i class="halflings-icon white zoom-in"></i></a></td>';
-							var trTail = '</tr>';
-							html += trHead + tdid + tdOderTime
-									+ tdTotal + tdIsCancel
-									+ tdProductName + btn + trTail;
+						var btn = '<td class="center "><a class="btn btn-success" href="#" onClick="seeOrderInfo('
+								+ item.id
+								+ ')"><i class="halflings-icon white zoom-in"></i></a></td>';
+						var trTail = '</tr>';
+						html += trHead + tdid + tdOderTime + tdTotal
+								+ tdIsCancel + tdProductName + btn + trTail;
 
+					});
+
+	$("#tbody").html(html);
+}
+
+function seeOrderInfo(id) {
+	layuiAlert("查看订单具体信息!");
+}
+function deleteContactInfo(id) {
+	layuiAlert("删除联系记录!");
+}
+
+//预约按钮点击事件
+$('#contactSubmitBtn').click(
+		function() {
+			var data = '{ "contactTime":"'
+					+ $("input[name='contactTime']").val() + '","content":"'
+					+ $("input[name='content']").val() + '","isChance":"'
+					+ $("select[name='isChance']").val()
+					+ '","subscribeTime":"'
+					+ $("input[name='subscribeTime']").val()
+					+ '","customerId":"' + id + '"}';
+			$.ajax({
+				type : "post",
+				forceSync : false,
+				url : "/contact/addContact",
+				contentType : "application/json; charset=utf-8",
+				data : data,
+				dataType : "json",
+				success : function(message) {
+					if (message.code == 0) {
+						layer.msg('预约成功', {
+							icon : 1,
+							time : 500
+						//1.2秒关闭（如果不配置，默认是3秒）
+						}, function() {
+							// 刷新页面
+							location.reload();
 						});
+					} else {
+						layuiAlert(message.msg);
+					}
+					return true;
+				},
+				error : function(message) {
+					layuiAlert("系统环境异常");
+					return false;
+				}
+			});
 
-		$("#tbody").html(html);
-	}
-	
-	function seeOrderInfo(id){
-		layuiAlert("查看订单具体信息!");
-	}
-	
+		});
+
+//删除联系信息
+function deleteContactInfo(id) {
+	var data = '{ "id":"' + id + '"}';
+	$.ajax({
+		type : "post",
+		async : true,
+		url : "/contact/deleteContact",
+		contentType : "application/json; charset=utf-8",
+		data : data,
+		dataType : "json",
+		success : function(message) {
+			if (message.code == 0) {
+				layer.msg('删除成功', {
+					icon : 1,
+					time : 500
+				// 1秒关闭（如果不配置，默认是3秒）
+				}, function() {
+					// 刷新页面
+					location.reload();
+				});
+			} else {
+				layuiAlert(message.msg);
+			}
+			return true;
+		},
+		error : function() {
+			layuiAlert("系统环境异常");
+			return false;
+		}
+	});
+}
+
 //关闭页面
 $("#closePage").click(function() {
 	var index = parent.layer.getFrameIndex(window.name);
