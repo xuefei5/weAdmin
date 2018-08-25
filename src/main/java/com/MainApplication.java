c78d70ac520a9dc5fd0c6c677bd96e7d547bc9db
yangsheng <1821332070@qq.com>
2018-08-25 18:02:50 +0800

    war包部署准备

--- src/main/java/com/MainApplication.java
+++ src/main/java/com/MainApplication.java
@@ -2,17 +2,25 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
- * @des    启动类，情景1，测试情景       情景2，部署情景（暂不开放）
+ * @des    启动类，情景1，测试情景       情景2，部署情景（暂不开放）SpringBootServletInitializer
  * @author yangsheng
  *
  */
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MainApplication.class);
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MainApplication.class, args);
     }
+
 }
