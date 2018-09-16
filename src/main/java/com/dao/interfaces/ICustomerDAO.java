eec40a7a9062efc2415baa3b982c644c4624e988
yangsheng <1821332070@qq.com>
2018-09-16 15:42:51 +0800

    客户基类修改-便于添加销售机会

--- src/main/java/com/dao/interfaces/ICustomerDAO.java
+++ src/main/java/com/dao/interfaces/ICustomerDAO.java
@@ -19,31 +19,31 @@ public interface ICustomerDAO{
 	public int insert(Customer customer);
 	
 	/*根据id查询客户对象*/
-	@Select("select * from customer where id = #{id} and state='0'")
+	@Select("select * from customer where id = #{id} and state!='1'")
 	public Customer qryById(@Param("id")int id);
 	
 	/*根据姓名模糊查询客户对象*/
-	@Select("select * from customer where name like CONCAT('%',#{name},'%') and state='0'")
+	@Select("select * from customer where name like CONCAT('%',#{name},'%') and state!='1'")
 	public List<Customer> qryByName(@Param("name")String name );
 	
 	/*查询所有客户对象*/
-	@Select("select * from customer where state='0'")
+	@Select("select * from customer where state!='1'")
 	public List<Customer> qryAll();
 	
 	/*获取客户总条数*/
-	@Select("select count(*) from customer where state='0'")
+	@Select("select count(*) from customer where state!='1'")
 	public int getCustomerAllCount();
 	
 	/*更新客户对象*/
-	@Update("UPDATE customer SET name = #{name},nickName = #{nickName},sex = #{sex},telephone = #{telephone},imgRef = #{imgRef},birthday = #{birthday},remarks = #{remarks},state = 0 where id = #{id}")
+	@Update("UPDATE customer SET name = #{name},nickName = #{nickName},sex = #{sex},telephone = #{telephone},imgRef = #{imgRef},birthday = #{birthday},remarks = #{remarks},state = '0' where id = #{id}")
 	public int update(Customer customer);
 	
 	/*删除客户对象*/
-	@Update("UPDATE customer SET state = 1 WHERE  id = #{id}")
+	@Update("UPDATE customer SET state = '1' WHERE  id = #{id}")
 	public int delete(@Param("id")int id);
 	
 	/*分页查询用户对象*/
-	@Select("select * from customer where state='0' order by addTime desc limit #{start} , #{end} ")
+	@Select("select * from customer where state != '1' order by addTime desc limit #{start} , #{end} ")
 	public List<Customer> qryCustomerByPageNum(@Param("start")int id,@Param("end")int end);
 	
 }
